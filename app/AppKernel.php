5d5b06fedbf4c97697639ebb5ee2ef9f6fa0d25c
Jerick Duguran <jerick.duguran@gmail.com>
2016-12-22 11:06:45 +0800

    - user, admin worked

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,24 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+			  
+			new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+			 
+			new FOS\UserBundle\FOSUserBundle(),
+			new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'), 
+
+			//Sonata Admin Dependencies
+			new Sonata\CoreBundle\SonataCoreBundle(),
+			new Sonata\BlockBundle\SonataBlockBundle(),
+			new Knp\Bundle\MenuBundle\KnpMenuBundle(),			
+			new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+
+			
+            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+			
+			//SonataAdminBundle
+			new Sonata\AdminBundle\SonataAdminBundle(),
+
             new AppBundle\AppBundle(),
         );
 
